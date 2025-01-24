b883fee5f522b46c554cd70c26ac1b1933c8df45
LaGameuse1923 <120399537+LaGameuse1923@users.noreply.github.com>
2025-01-24 19:38:20 +0100

    Start

---
+++ Assets/script/MovePlayer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    public float vitesse = 3;
+    private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
+    public float jump = 3;
+    private Vector3 change;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMovement = Input.GetAxis("Horizontal") * vitesse;
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+
+        DeplacementPlayer(horizontalMovement);
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //_rb.AddForce
+            _rb.velocity = _rb.velocity * vitesse;
+        }
+        */
+    }
+
+    public void DeplacementPlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, _rb.velocity.y);
+        _rb.velocity = Vector3.SmoothDamp(_rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+}
